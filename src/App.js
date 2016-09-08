b458d5d1d3a629f556f9910153ced8d3cc959835
Peranat Dayananda <peranatdayananda@gmail.com>
2016-09-08 11:04:37 +0700

    (chore) Organise client folder.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import Button from './components/button';
+import Button from './stateless/button';
 import './App.css';
 
 injectTapEventPlugin();
